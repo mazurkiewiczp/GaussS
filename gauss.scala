444b6b65dc7da21ca90d3b8d4a1a5559c3280fef
mazurkiewiczp <p.maz6789@gmail.com>
2016-06-09 01:42:14 +0200

    Gauss-Seidel

--- gauss.scala
+++ gauss.scala
@@ -1,8 +1,42 @@
 import scala.io.Source
 import java.io.FileNotFoundException
 import java.io.IOException
+import java.lang.ArrayIndexOutOfBoundsException
+import util.Random
+import math.abs
 
-def gauss(A:Array[Array[Double]],Y:Array[Double]):Array[Double] = {
+def gaussSeidel(A:Array[Array[Double]],Y:Array[Double]):Array[Double] = {
+	var X = new Array[Double](A.length)
+	var P = new Array[Double](A.length)
+	var d = A.length
+	val MAX = 100				
+	var count = 0 
+	val EPS = 0.000001			
+	var stop = false
+	var q = 0.0
+
+	do
+	{
+	count += 1
+
+	for (i <- 0 until A.length){
+		q = 0.0
+		for (j <- 0 until A.length) if(i!=j) q += A(i)(j)*P(j)
+		X(i) = (Y(i)-q)/A(i)(i)
+		}
+
+	d = A.length
+	for (k <- 0 until X.length){
+		if (abs(P(k)-X(k))<EPS) d-=1
+		P(k) = X(k)
+		}
+	if (d==0) stop = true
+
+	} while (!stop && count <= MAX)
+	X
+}
+
+def gaussElimination(A:Array[Array[Double]],Y:Array[Double]):Array[Double] = {
 	for(i <- 0 until A.length){
 		for(j <- i+1 until A.length){
 			var ratio = A(j)(i)/A(i)(i)
@@ -22,7 +56,7 @@ def gauss(A:Array[Array[Double]],Y:Array[Double]):Array[Double] = {
 	X
 }
 
-def printMatrix(X:Array[Double]) = {
+def printVector(X:Array[Double]) = {
 	for (i <- 0 until X.length)
 		println("X_" + (i+1) + " = " + X(i))	
 }
@@ -30,16 +64,15 @@ def printMatrix(X:Array[Double]) = {
 def doTheThing(A:Array[Array[Double]],Y:Array[Double]) = {
 	if (args.length < 2 || (args.length>1 && args(1) != "gs")){
 		println("Gaussian Elimination (default)")
-		val ans = gauss(A,Y)
-		printMatrix(ans)
+		val ans = gaussElimination(A,Y)
+		printVector(ans)
 	}
 	else {
 		println("Gauss-Seidel method")
-		println("TODO ¯\\_(ツ)_/¯")
+		val ans = gaussSeidel(A,Y)
+		printVector(ans)
 	}
 }
-
-
 
 if (args.length == 0){
 println("No arguments provided. Need file path.")
@@ -59,6 +92,5 @@ try {
 } catch {
   case ex: FileNotFoundException => println("Couldn't find that file.")
   case ex: IOException => println("Had an IOException trying to read that file.")
+  case ex: ArrayIndexOutOfBoundsException => println("Data should have N rows and N+1 columns.")
 }
-
-
